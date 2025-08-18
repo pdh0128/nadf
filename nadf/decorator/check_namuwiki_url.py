d89f14a1a51ba13af0ed7b4962897fd1d04e195a
pdh0128 <pdh0128a@gmail.com>
2025-08-19 08:50:39 +0900

    fix :: namuwiki url check decorator 수정

--- nadf/decorator/check_namuwiki_url.py
+++ nadf/decorator/check_namuwiki_url.py
@@ -1,18 +1,36 @@
-from http.client import HTTPException
-
+import asyncio
+import functools
+import inspect
+from typing import Optional
 from nadf.exception.not_namuwiki_exception import NotNamuwikiException
 
+def _extract_url(func, args, kwargs, param: str = "url") -> Optional[str]:
+    """함수 호출 인자에서 'url' 값을 찾아 반환"""
+    try:
+        bound = inspect.signature(func).bind_partial(*args, **kwargs)
+        return bound.arguments.get(param)
+    except TypeError:
+        return kwargs.get(param)
 
-def check_namuwiki_url(func):
-    def wrapper(*args, **kwargs):
-        try:
-            url = kwargs.get("url")
+def _validate_namuwiki(url: Optional[str]):
+    if not url or "namu.wiki" not in str(url):
+        raise NotNamuwikiException()
 
-            if not url or "namu.wiki" not in url:
-                raise NotNamuwikiException()
-
-            return func(*args, **kwargs)
-        except NotNamuwikiException as e:
-            print("error : ", e.message)
-            print("status code:", e.status_code)
-    return wrapper
+def check_namuwiki_url(param: str = "url"):
+    """데코레이터: url 인자가 반드시 namu.wiki 포함해야 함"""
+    def decorator(func):
+        if asyncio.iscoroutinefunction(func):
+            @functools.wraps(func)
+            async def aw(*args, **kwargs):
+                url = _extract_url(func, args, kwargs, param)
+                _validate_namuwiki(url)
+                return await func(*args, **kwargs)
+            return aw
+        else:
+            @functools.wraps(func)
+            def sw(*args, **kwargs):
+                url = _extract_url(func, args, kwargs, param)
+                _validate_namuwiki(url)
+                return func(*args, **kwargs)
+            return sw
+    return decorator
